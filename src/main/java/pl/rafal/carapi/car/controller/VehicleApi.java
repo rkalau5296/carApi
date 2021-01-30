3512f5c118f51f4a78e0662b20c15113dcd13d54
rkalau <rafal.kalata.itservice@gmail.com>
2021-01-30 23:39:59 +0100

    commit

--- src/main/java/pl/rafal/carapi/car/controller/VehicleApi.java
+++ src/main/java/pl/rafal/carapi/car/controller/VehicleApi.java
@@ -2,10 +2,10 @@ package pl.rafal.carapi.car.controller;
 
 import org.springframework.web.bind.annotation.*;
 import pl.rafal.carapi.car.model.Vehicle;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/vehicles")
@@ -18,6 +18,9 @@ public class VehicleApi {
         vehicles.add(new Vehicle(1L, "BMW","850","black"));
         vehicles.add(new Vehicle(2L, "Ford","Fiesta","white"));
         vehicles.add(new Vehicle(3L, "Fiat","Panda","blue"));
+        vehicles.add(new Vehicle(4L, "Fiat","Panda","blue"));
+        vehicles.add(new Vehicle(5L, "Fiat","Panda","blue"));
+
     }
 
 
@@ -33,13 +36,8 @@ public class VehicleApi {
 
     @RequestMapping(value = "/vehicles/vehicles/{color}", method = RequestMethod.GET)
     public List<Vehicle> getVehicleByColor(@PathVariable String color){
-        List<Vehicle> vehicleByColor = new ArrayList<>();
 
-        for (Vehicle vehicle : vehicles) {
-            if (vehicle.getColor().equals(color))
-                vehicleByColor.add(vehicle);
-        }
-        return vehicleByColor;
+        return vehicles.stream().filter(vehicle -> vehicle.getColor().equals(color)).collect(Collectors.toList());
     }
 
     @RequestMapping(method = RequestMethod.POST)
